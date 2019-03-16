23f169b21f825305f6e85c830e85be65aac7e1fd
Jordan <42647636+jdspell@users.noreply.github.com>
2019-03-16 15:49:05 -0400

    trying to add get functionality to delete request

--- api/house-info/houseRouter.js
+++ api/house-info/houseRouter.js
@@ -56,16 +56,17 @@ router.get('/house/:id', (req, res) => {
       const count = await Houses.remove(req.params.id);
       if (count > 0) {
         res.status(200).json({ message: 'House has been removed' });
-        let { id } = req.params
-        Houses.findById(id)
-          .then(house => {   
-            res.status(201).json(house);
-          .catch(error => {
-            res.status(500).json(error);
-      });
       } else {
         res.status(404).json({ message: 'House could not be found' });
       }
+      let { id } = req.params
+      Houses.findById(id)
+        .then(house => {   
+          res.status(201).json(house);
+        })
+        .catch(error => {
+          res.status(500).json(error);
+      });
     } catch (error) {
       console.log(error);
       res.status(500).json({
